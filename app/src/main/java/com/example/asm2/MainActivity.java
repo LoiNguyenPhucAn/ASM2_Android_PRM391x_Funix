6e948f813ee481e69953320982abaf720ac92f70
LoiNguyenPhucAn <lnphucan@gmail.com>
2021-12-03 09:30:43 +0700

    Update Drawer layout
    hiệu chỉnh màu toggle button thành màu trắng
    
    thêm lắng nghe sự kiện click on navigation item
    
    hiển thị action bar title tương ứng khi chọn item trong navigation

--- app/src/main/java/com/example/asm2/MainActivity.java
+++ app/src/main/java/com/example/asm2/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.asm2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,12 +17,15 @@ import android.widget.TextView;
 
 import com.example.asm2.databinding.ActivityMainBinding;
 import com.example.asm2.databinding.NavHeaderBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mainBinding;
 
     ActionBarDrawerToggle toggle;
+    DrawerLayout drawerMain;
+    NavigationView navigationDrawerMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +41,37 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(true);
 
 
-        DrawerLayout drawerMain = mainBinding.drawerMain;
+        drawerMain = mainBinding.drawerMain;
         toggle = new ActionBarDrawerToggle(this, drawerMain, R.string.Open, R.string.Close);
         drawerMain.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationDrawerMain = mainBinding.navigationItemleft;
+        navigationDrawerMain.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.itemSMS:
+                        getSupportActionBar().setTitle(R.string.peding_sms);
+                        break;
+
+                    case R.id.itemPhone:
+                        getSupportActionBar().setTitle(R.string.phone_call);
+                        break;
+
+                    case R.id.itemAlarm:
+                        getSupportActionBar().setTitle(R.string.alarm);
+                        break;
+                }
+
+                drawerMain.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
 
 
     }
@@ -50,21 +82,6 @@ public class MainActivity extends AppCompatActivity {
 
         if (toggle.onOptionsItemSelected(item)) {
             return true;
-        }
-
-        switch (item.getItemId()) {
-
-            case R.id.itemSMS:
-                getSupportActionBar().setTitle(R.string.peding_sms);
-                break;
-
-            case R.id.itemPhone:
-                getSupportActionBar().setTitle(R.string.phone_call);
-                break;
-
-            case R.id.itemAlarm:
-                getSupportActionBar().setTitle(R.string.alarm);
-                break;
         }
 
         return super.onOptionsItemSelected(item);
